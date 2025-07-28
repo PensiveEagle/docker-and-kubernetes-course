293e3c3c5feddf19a80cb55bdd7499c879eae40f
PensiveEagle <halljf96@gmail.com>
2025-07-28 15:35:10 +0100

    Added missing semi-colon

--- kub-network/tasks-api/tasks-app.js
+++ kub-network/tasks-api/tasks-app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
   res.setHeader('Access-Control-Allow_Headers', 'Content-Type,Authorization');
   next();
-})
+});
 
 const extractAndVerifyToken = async (headers) => {
   if (!headers.authorization) {
