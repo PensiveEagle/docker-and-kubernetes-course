d5d0db1e38b9313db732f23fd6019cf92793fd92
PensiveEagle <halljf96@gmail.com>
2025-07-28 15:48:12 +0100

    Corrected _ to -
    Was using Access-Control-Allow_Headers instead of Access-Control-Allow-Headers

--- kub-network/tasks-api/tasks-app.js
+++ kub-network/tasks-api/tasks-app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.setHeader('Access-Control-Allow_Headers', 'Content-Type,Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   next();
 });
 
