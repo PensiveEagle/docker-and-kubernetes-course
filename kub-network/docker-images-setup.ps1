4b4ca42ddbb55fe9dbc1677667459cc7de1e2945
PensiveEagle <halljf96@gmail.com>
2025-07-29 11:05:54 +0100

    Resolved

--- kub-network/docker-images-setup.ps1
+++ kub-network/docker-images-setup.ps1
@@ -3,16 +3,16 @@ $repository_name = "kub-net-demo"
 
 docker build -t $registry_account_name/$repository_name":users-api-1" ./users-api
 
-# docker build -t $registry_account_name/$repository_name:tasks-api- ./tasks-api
+docker build -t $registry_account_name/$repository_name":tasks-api-1" ./tasks-api
 
-# docker build -t $registry_account_name/$repository_name:auth-api-1 ./auth-api
+docker build -t $registry_account_name/$repository_name":auth-api-1" ./auth-api
 
-# docker build -t $registry_account_name/$repository_name:frontend-1 ./frontend
+docker build -t $registry_account_name/$repository_name":frontend-1" ./frontend
 
-# docker push $registry_account_name/$repository_name:users-api-1
+docker push $registry_account_name/$repository_name":users-api-1"
 
-# docker push $registry_account_name/$repository_name:tasks-api-1
+docker push $registry_account_name/$repository_name":tasks-api-1"
 
-# docker push $registry_account_name/$repository_name:auth-api-1
+docker push $registry_account_name/$repository_name":auth-api-1"
 
-# docker push $registry_account_name/$repository_name:frontend-1
+docker push $registry_account_name/$repository_name":frontend-1"
